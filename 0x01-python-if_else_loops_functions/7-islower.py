acb09b55a6f32eb29001f5c79f502ff5e61c1e66
Tsehafetizaz <wendetefe@gmail.com>
2023-11-28 09:25:36 -0800

    daf

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
+    if len(c) != 1:  # Check if the input is not a single character
+        return False  # or handle it differently if needed
     return 'a' <= c <= 'z'
