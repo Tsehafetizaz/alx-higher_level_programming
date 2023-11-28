d931ba7c4b93b7796dae09978c8d881103bef5f4
Tsehafetizaz <wendetefe@gmail.com>
2023-11-28 10:11:01 -0800

    test

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-for i in range(99):
-        print("{:02d}".format(i), end=", ")
-        print("{:02d}".format(99))
+for num in range(100):
+    if num < 10:
+        print("0{}, ".format(num), end='')
+    else:
+        if num == 99:
+            print("{}".format(num))
+        else:
+            print("{}, ".format(num), end='')
