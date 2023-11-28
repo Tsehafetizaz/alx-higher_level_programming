74106623f09bb76ab3be4e73373366531d198e15
Tsehafetizaz <wendetefe@gmail.com>
2023-11-28 05:09:53 -0800

    8

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-# Loop through numbers from 0 to 98
-for number in range(99):
-    # Print the number in decimal and hexadecimal formats
-    print(f"{number} = 0x{number:x}")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-# Loop through the first digit (0 to 8)
-for first_digit in range(9):
-    # Loop through the second digit (1 greater than the first digit to 9)
-    for second_digit in range(first_digit + 1, 10):
-        # Use an f-string to format and print the combination
-        print(f"{first_digit}{second_digit}", end=", " if first_digit < 8 or second_digit < 9 else "\n")
