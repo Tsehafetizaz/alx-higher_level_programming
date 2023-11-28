dfde001d4e75e47776587bdec82eb286f7f9cfb1
Tsehafetizaz <wendetefe@gmail.com>
2023-11-28 10:16:05 -0800

    test

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,18 +1,7 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 def remove_char_at(str, n):
-    # Check if n is a valid index within the string
-    if n >= 0 and n < len(str):
-        # Use string slicing to remove the character at position n
-        return str[:n] + str[n + 1:]
-    else:
-        # Return the original string if n is out of range
+    if n < 0:
         return str
-
-    if __name__ == "__main__":
-        # Test cases
-        print(remove_char_at("Best School", 3))
-        print(remove_char_at("Chicago", 2))
-        print(remove_char_at("C is fun!", 0))
-        print(remove_char_at("School", 10))
-        print(remove_char_at("Python", -2))
+    else:
+        str = str[0:n] + str[n+1:]
+        return str
