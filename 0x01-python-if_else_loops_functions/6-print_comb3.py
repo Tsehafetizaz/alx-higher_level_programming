47b341bd6e028cae000eb6f453bd6f3874d4ea24
Tsehafetizaz <wendetefe@gmail.com>
2023-11-28 09:54:34 -0800

    test

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(9):
-    for j in range(i + 1, 10):
-        if i * 10 + j < 89:
-            print("{:d}{:d}".format(i, j), end=", ")
-            print("{:d}".format(89))
+for i in range(10):
+        for j in range(i + 1, 10):
+                    print(f"{i}{j}, ", end='')
+
+                    print(f"98, 99")
