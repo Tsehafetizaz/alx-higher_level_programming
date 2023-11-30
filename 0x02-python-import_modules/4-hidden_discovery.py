c175825dcd13e6d9edd65ec11b86457c98e724c6
Tsehafetizaz <wendetefe@gmail.com>
2023-11-30 04:34:18 -0800

    tesla

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,11 +2,13 @@
 
 import hidden_4
 
+
 def print_names():
     names = dir(hidden_4)
     for name in names:
         if not name.startswith('__'):
             print(name)
+
 
 if __name__ == "__main__":
     print_names()
