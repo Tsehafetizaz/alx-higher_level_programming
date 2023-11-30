5c6cf40645e1673d740513ce6303d9350724d0ba
Tsehafetizaz <wendetefe@gmail.com>
2023-11-30 05:02:42 -0800

    hmm

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-import string
-print(*map(chr, range(ord(string.ascii_uppercase[0]), ord(string.ascii_uppercase[-1])+1)), sep='')
+from string import ascii_uppercase
+print(ascii_uppercase)
