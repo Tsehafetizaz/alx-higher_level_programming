e6405951b0bfb7edd26d36ce8b4cc4c694381e4c
Tsehafetizaz <wendetefe@gmail.com>
2023-11-30 03:21:18 -0800

    tesa

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
-import sys
-
-# Get the number of arguments (excluding the script name)
-num_arguments = len(sys.argv) - 1  # Subtract 1 for the script name itself
-
-# Get the list of arguments (excluding the script name)
-arguments = sys.argv[1:]
-
-if num_arguments == 0:
-    print("0 arguments.")
-elif num_arguments == 1:
-    print("1 argument:")
-else:
-    print(f"{num_arguments} arguments:")
-
-    # Print each argument with its position
-    for i, arg in enumerate(arguments, start=1):
-        print(f"{i}: {arg}")
+if __name__ == "__main__":
+    import sys
+    argLen = len(sys.argv)
+    if argLen == 1:
+        print("{} arguments.".format(argLen - 1))
+    elif argLen == 2:
+        print("{} argument:".format(argLen - 1))
+    else:
+        print("{} arguments:".format(argLen - 1))
+        for i in range(1, argLen):
+            print("{}: {}".format(i, sys.argv[i]))
