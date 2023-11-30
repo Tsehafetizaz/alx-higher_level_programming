d6f47278772959c3c77a7b1844627fa8d2568252
Tsehafetizaz <wendetefe@gmail.com>
2023-11-30 04:28:46 -0800

    yra

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+import sys
+
+
+def main():
+    arguments = sys.argv[1:]
+    total = sum(int(arg) for arg in arguments)
+    print(total)
+
+
 if __name__ == "__main__":
-    import sys
-    result = 0
-    for i in sys.argv[1:]:
-        result += int(i)
-        print("{}".format(result))
+    main()
