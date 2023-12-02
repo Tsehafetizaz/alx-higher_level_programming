8d3ee802c568e2ed1d4ff626e617758f61224adb
Tsehafetizaz <wendetefe@gmail.com>
2023-12-02 02:55:11 -0800

    halo

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,6 +4,6 @@ def print_list_integer (my_list=[]):
     for i in my_list:
         print("{:d}".format(i))
 
-if __name__=="__main__"
+if __name__=="__main__":
     my_list=[0,1,2,3,4,5]
     print_list_integer (my_list)
