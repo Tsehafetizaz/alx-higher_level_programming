16818bb0d7c0e99eec8522db6ca709d2a839d688
Tsehafetizaz <wendetefe@gmail.com>
2023-12-04 10:38:02 -0800

    fad

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -8,6 +8,6 @@ def multiple_returns(sentence):
 
 
 if __name__ == "__main__":
-        sentence = "At school, I learnt C!"
-        length, first = multiple_returns(sentence)
-        print("Length: {:d} - First character: {}".format(length, first))
+    sentence = "At school, I learnt C!"
+    length, first = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(length, first))
