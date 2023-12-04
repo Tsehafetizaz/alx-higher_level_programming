89980a720d0727e63650287924cbbc36d91e6c8d
Tsehafetizaz <wendetefe@gmail.com>
2023-12-04 09:36:44 -0800

    daj

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,8 +6,3 @@ def no_c(my_string):
         if char != "c" and char != "C":
             new_string += char
     return new_string
-
-
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
