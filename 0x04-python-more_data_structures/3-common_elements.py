549494a502b3db3abaf5140eb361f062b4e503a2
Tsehafetizaz <wendetefe@gmail.com>
2023-12-06 04:30:06 -0800

    gak

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    common_set = set()
-    for element in set_1:
-        if element in set_2:
-            common_set.add(element)
-        return common_set
+    common_set = set_1.intersection(set_2)
+    return common_set
 
 
 if __name__ == "__main__":
