4ad99f53d5a61ca5ca621ce571ca549db1e4ea8d
Tsehafetizaz <wendetefe@gmail.com>
2023-12-06 11:39:35 -0800

    aglkds

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for element in row:
-            new_row.append(element * element)
-        new_matrix.append(new_row)
+
+    new_matrix = [[i ** 2 for i in row] for row in matrix]
     return new_matrix
 
 
