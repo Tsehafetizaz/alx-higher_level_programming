e8c5bee1bff384d79b0d0a5fd697712a1f83a44b
Tsehafetizaz <wendetefe@gmail.com>
2023-12-06 12:18:30 -0800

    gaga

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
+    return (list(map(lambda row: list(map(lambda x: x**2, row)), matrix)))
 
 
 if __name__ == "__main__":
