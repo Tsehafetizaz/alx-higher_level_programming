5f48ebd8dc1d5418041fcfe15e1a10bc1fa3dc25
Tsehafetizaz <wendetefe@gmail.com>
2023-12-06 13:13:27 -0800

    daw

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,17 +3,7 @@ def square_matrix_simple(matrix=[]):
     if not matrix:
         return []
 
-    # Create a new matrix with the same size as the input matrix
-    new_matrix = []
-
-    # Iterate over each row in the matrix
-    for row in matrix:
-        # Use a list comprehension to square each element in the row
-        squared_row = [x ** 2 for x in row]
-        # Add the squared row to the new matrix
-        new_matrix.append(squared_row)
-
-    return new_matrix
+    return [[element ** 2 for element in row] for row in matrix]
 
 
 if __name__ == "__main__":
