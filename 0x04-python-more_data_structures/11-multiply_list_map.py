a68ba3f32dde6485bbbc0b2f3462fe53bfe439f4
Tsehafetizaz <wendetefe@gmail.com>
2023-12-06 12:08:03 -0800

    ga

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda i: i * number, my_list))
-
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 6]
-    new_list = multiply_list_map(my_list, 4)
-    print(new_list)  # Expected output: [4, 8, 12, 16, 24]
-    print(my_list)   # Expected output: [1, 2, 3, 4, 6]
+    return list(map(lambda i: i * number, my_list)
