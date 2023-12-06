c044ab11554be2f8e3e0d776026954f33c0162c2
Tsehafetizaz <wendetefe@gmail.com>
2023-12-06 12:33:57 -0800

    sga

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
 
-    new_matrix = [[i ** 2 for i in row] for row in matrix]
-    return new_matrix
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda w: w**2, matrix[i]))
+
+    return (new_matrix)
 
 
 if __name__ == "__main__":
