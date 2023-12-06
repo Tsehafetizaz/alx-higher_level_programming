378cf60ea9a3c8c6a4227bdf6097e753e5c64b0f
Tsehafetizaz <wendetefe@gmail.com>
2023-12-06 03:46:44 -0800

    gags

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    unique = set()
+    for i in my_list:
+        unique.add(i)
+    result = sum(unique)
+    return result
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 1, 4, 2, 5]
+    result = uniq_add(my_list)
+    print("Result: {:d}".format(result))
