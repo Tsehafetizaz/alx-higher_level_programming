db1a54a23bfe08d805e71b41a127deac3bf7e45c
Tsehafetizaz <wendetefe@gmail.com>
2023-12-06 12:43:41 -0800

    ag

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,16 +3,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-
-    best_key = None
-    best_val = float('-inf')
-
-    for key, value in a_dictionary.items():
-        if value > best_val:
-            best_val = value
-            best_key = key
-
-            return best_key
+    return max(a_dictionary, key=a_dictionary.get)
 
 
 if __name__ == "__main__":
