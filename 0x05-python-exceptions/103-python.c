ce02aa298709c8d5d926190f8f7e932ab8dd0c31
Tsehafetizaz <wendetefe@gmail.com>
2023-12-18 11:53:10 -0800

    wait

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -2,21 +2,79 @@
 #include <object.h>
 #include <listobject.h>
 #include <bytesobject.h>
+#include <floatobject.h>
 
+/**
+ * print_python_list - Print basic info about Python lists
+ * @p: PyObject list
+ */
 void print_python_list(PyObject *p)
 {
-	/* Check if p is a valid list object */
-	/* Print list information or error message */
+	Py_ssize_t size, i;
+	PyObject *item;
+
+	printf("[*] Python list info\n");
+	if (!PyList_Check(p))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+
+	size = PyList_GET_SIZE(p);
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GET_ITEM(p, i);
+		printf("Element %zd: %s\n", i, item->ob_type->tp_name);
+	}
 }
 
+/**
+ * print_python_bytes - Print basic info about Python byte objects
+ * @p: PyObject byte object
+ */
 void print_python_bytes(PyObject *p)
 {
-	/* Check if p is a valid bytes object */
-	/* Print bytes information or error message */
+	Py_ssize_t size, i;
+	char *string_representation;
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	size = PyBytes_Size(p);
+	string_representation = PyBytes_AsString(p);
+	printf("  size: %zd\n", size);
+	printf("  trying string: %s\n", string_representation);
+
+	printf("  first %zd bytes:", size < 10 ? size + 1 : 10);
+	for (i = 0; i < size && i < 10; i++)
+	{
+		printf(" %02x", string_representation[i] & 0xff);
+	}
+	printf("\n");
 }
 
+/**
+ * print_python_float - Print basic info about Python float objects
+ * @p: PyObject float object
+ */
 void print_python_float(PyObject *p)
 {
-	/* Check if p is a valid float object */
-	/* Print float information or error message */
+	double value;
+
+	printf("[.] float object info\n");
+	if (!PyFloat_Check(p))
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		return;
+	}
+
+	value = PyFloat_AS_DOUBLE(p);
+	printf("  value: %lf\n", value);
 }
