e88ec6d7f5389238e3ee9e860ccb9644e500cc94
Tsehafetizaz <wendetefe@gmail.com>
2023-12-18 11:31:01 -0800

    anymore

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,22 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
+
+void print_python_list(PyObject *p)
+{
+	/* Check if p is a valid list object */
+	/* Print list information or error message */
+}
+
+void print_python_bytes(PyObject *p)
+{
+	/* Check if p is a valid bytes object */
+	/* Print bytes information or error message */
+}
+
+void print_python_float(PyObject *p)
+{
+	/* Check if p is a valid float object */
+	/* Print float information or error message */
+}
