97c68190c90971fd3f95eb27dc8db8dac6b44240
Tsehafetizaz <wendetefe@gmail.com>
2023-12-18 11:01:10 -0800

    hello

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,7 @@ def safe_print_list(my_list=[], x=0):
         return count
 
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
         my_list = [1, 2, 3, 4, 5]
 
         nb_print = safe_print_list(my_list, 2)
