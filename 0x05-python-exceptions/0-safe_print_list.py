d88bb8d68757b1e80aaff321bbce4da1b0692fda
Tsehafetizaz <wendetefe@gmail.com>
2023-12-18 11:34:41 -0800

    thousandmore

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         except IndexError:
             break
-        print()
-        return count
+    print()
+    return count
 
 
 if __name__ == "__main__":
