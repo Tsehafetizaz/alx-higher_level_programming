45a81a8a21d22dee5e679ed0a204efa1a7ed414e
Tsehafetizaz <wendetefe@gmail.com>
2024-01-08 01:52:16 -0800

    operator

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Defines an inherited class-checking function."""
+
 
 def inherits_from(obj, a_class):
     """
@@ -14,15 +16,3 @@ def inherits_from(obj, a_class):
               otherwise False.
     """
     return isinstance(obj, a_class) and type(obj) != a_class
-
-
-if __name__ == "__main__":
-    # Test cases
-
-    a = True
-    if inherits_from(a, int):
-        print("{} inherited from class {}".format(a, int.__name__))
-    if inherits_from(a, bool):
-        print("{} inherited from class {}".format(a, bool.__name__))
-    if inherits_from(a, object):
-        print("{} inherited from class {}".format(a, object.__name__))
