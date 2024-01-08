e21b1af62f408e6c97a4ab4cef272acada9ebf88
Tsehafetizaz <wendetefe@gmail.com>
2024-01-08 01:39:39 -0800

    nono

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,17 +11,3 @@ class MyList(list):
         Prints the elements of the list in ascending order.
         """
         print(sorted(self))
-
-
-if __name__ == "__main__":
-    # Test cases
-
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
