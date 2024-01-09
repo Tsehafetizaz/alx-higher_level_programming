64bdd2277a9b750530c15906865bf07843dff95e
Tsehafetizaz <wendetefe@gmail.com>
2024-01-09 08:53:19 -0800

    check

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""writes a string to a text file returns the number of characters written"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file returns the number of characters written.
+
+    Args:
+    filename (str): The path to the file where the text will be written.
+    text (str): The text to be written to the file.
+
+    Returns:
+    int: The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
