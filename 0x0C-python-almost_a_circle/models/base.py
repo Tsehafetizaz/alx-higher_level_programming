619e7737788b82d90f1b406a0172246f1d169525
Tsehafetizaz <wendetefe@gmail.com>
2024-01-15 12:19:59 -0800

    dont

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 Module for the Base class.
 """
 
+import csv
 import json
 import os
 
@@ -46,10 +47,9 @@ class Base:
             list_objs (list): A list of instances that inherit from Base.
         """
         filename = "{}.json".format(cls.__name__)
-        list_dicts = [
-            obj.to_dictionary()
-            for obj in list_objs
-        ] if list_objs else []
+        list_dicts = (
+            [obj.to_dictionary() for obj in list_objs] if list_objs else []
+        )
         json_string = cls.to_json_string(list_dicts)
         with open(filename, 'w') as file:
             file.write(json_string)
@@ -101,3 +101,49 @@ class Base:
             list_dicts = cls.from_json_string(file.read())
 
         return [cls.create(**d) for d in list_dicts]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        Serialize a list of objects to a CSV file.
+        Args:
+            list_objs (list): A list of instances that inherit from Base.
+        """
+        filename = "{}.csv".format(cls.__name__)
+        with open(filename, 'w', newline='') as csvfile:
+            if cls.__name__ == "Rectangle":
+                fieldnames = ['id', 'width', 'height', 'x', 'y']
+            elif cls.__name__ == "Square":
+                fieldnames = ['id', 'size', 'x', 'y']
+
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for obj in list_objs:
+                writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        Deserialize a list of objects from a CSV file.
+        Returns:
+            list: A list of instances.
+        """
+        filename = f"{cls.__name__}.csv"
+        if not os.path.exists(filename):
+            return []
+
+        with open(filename, 'r', newline='') as csvfile:
+            if cls.__name__ == "Rectangle":
+                fieldnames = ['id', 'width', 'height', 'x', 'y']
+            elif cls.___name__ == "Square":
+                fieldnames = ['id', 'size', 'x', 'y']
+
+            reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+            list_dicts = [row for row in reader]
+
+            # Converting string values to integers where necessary
+            for d in list_dicts:
+                for key, value in d.items():
+                    d[key] = int(value)
+
+            return [cls.create(**d) for d in list_dicts]
