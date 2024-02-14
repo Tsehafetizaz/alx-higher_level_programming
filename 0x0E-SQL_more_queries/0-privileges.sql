dde2c002f5855df7150594c6c8366bc6be60293a
Tsehafetizaz <wendetefe@gmail.com>
2024-02-14 08:24:03 -0800

    halal

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+-- List all privileges for user 'user_0d_1' on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Separator for clarity
+SELECT '----';
+
+-- List all privileges for user 'user_0d_2' on localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
