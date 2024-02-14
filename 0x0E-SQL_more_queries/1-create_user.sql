a36bd83f570ea1ef9603882e8f290c1638cd5bd1
Tsehafetizaz <wendetefe@gmail.com>
2024-02-14 09:48:18 -0800

    chinkilat

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,8 @@
---create user_0d_1 with all privileges and specified password, without failing if user exists
+-- Check if the user 'user_0d_1' already exists and create it if not
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Grant all privileges to 'user_0d_1' on all databases and tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply the changes made by the GRANT statement
+FLUSH PRIVILEGES;
