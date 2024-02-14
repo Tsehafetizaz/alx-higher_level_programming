1cd825b0053ff572c8f7b4fa20993ee7486055cb
Tsehafetizaz <wendetefe@gmail.com>
2024-02-14 09:49:17 -0800

    milli

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
---create database hbtn_0d_2 and user user_0d_2 with SELECT privilege on hbtn_0d_2, without failing if either exists
+-- Create database hbtn_0d_2 and user user_0d_2 with SELECT privilege on hbtn_0d_2, without failing if either exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
