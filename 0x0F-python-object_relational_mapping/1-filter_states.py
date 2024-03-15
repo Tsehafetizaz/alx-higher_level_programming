b17ae64e28d2a94731e17e8939ecb6d51dc59e5c
Tsehafetizaz <wendetefe@gmail.com>
2024-03-15 01:43:03 -0700

    alpha

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This script lists states with names starting with 'N' from the hbtn_0e_0_usa .
+Usage: ./1-filter_states.py <mysql username> <mysql password> <database name>
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
