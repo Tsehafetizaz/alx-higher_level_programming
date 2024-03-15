3e5a58bf8ec296db6179ae0578841750fccd6558
Tsehafetizaz <wendetefe@gmail.com>
2024-03-15 11:34:10 -0700

    jaz

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for city, state in session.query(City, State).join(State).order_by(City.id).all():
+    for city, state in session.query(City, State) \
+            .join(State).order_by(City.id).all():
         print(f"{state.name}: ({city.id}) {city.name}")
 
     session.close()
