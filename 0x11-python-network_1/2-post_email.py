402d9fdcc93e72cb70abfce80229c07af6038ab7
Tsehafetizaz <wendetefe@gmail.com>
2024-04-02 06:36:29 -0700

    hello

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-import urllib.request
+"""Sends a POST request with an email parameter and response body."""
+
 import urllib.parse
+import urllib.request
 import sys
 
-if __name__ == "__main__":
+
+def main():
+    """Main function to send a POST request with email parameter."""
     url = sys.argv[1]
-    email = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(email)
-    data = data.encode('ascii')
-    with urllib.request.urlopen(url, data) as response:
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data=data)
+    with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
+
+
+if __name__ == "__main__":
+    main()
