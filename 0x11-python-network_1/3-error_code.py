388f60b060ea0ef5a9d0a6e1d5cf9094f468f41d
Tsehafetizaz <wendetefe@gmail.com>
2024-04-02 06:38:06 -0700

    hello

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-import urllib.request
+"""Displays the body of the response or error code if an HTTPError occurs."""
+
 import urllib.error
+import urllib.request
 import sys
 
-if __name__ == "__main__":
+
+def main():
+    """Main function to retrieve and display response body or error code."""
     url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print(f"Error code: {e.code}")
+        print("Error code:", e.code)
+
+
+if __name__ == "__main__":
+    main()
