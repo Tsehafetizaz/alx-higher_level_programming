57061f3eada748caf2e0e6da348a6e7762a01aab
Tsehafetizaz <wendetefe@gmail.com>
2024-04-02 06:48:18 -0700

    hello

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
+"""Sends a POST request with a letter parameter """
+
 import requests
 import sys
 
-if __name__ == "__main__":
-    q = sys.argv[1] if len(sys.argv) > 1 else ""
-    response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+
+def main():
+    """Main function to send a POST request with letter parameter."""
+    if len(sys.argv) < 2:
+        q = ''
+    else:
+        q = sys.argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+    response = requests.post(url, data={'q': q})
     try:
         json_response = response.json()
         if json_response:
-            print(f"[{json_response.get('id')}] {json_response.get('name')}")
+            print("[{}] {}".format(json_response.get('id'),
+                                   json_response.get('name')))
         else:
             print("No result")
     except ValueError:
         print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    main()
