4a4ee93340587ad14896d3a6be15446ee54cc475
Tsehafetizaz <wendetefe@gmail.com>
2024-04-02 06:52:21 -0700

    hello

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
+"""Uses GitHub API to list recent commits of a repository by a user."""
+
 import requests
 import sys
 
-if __name__ == "__main__":
-    repo_name = sys.argv[1]
+
+def main():
+    """Main function to fetch and display recent commits of a repository."""
+    repo = sys.argv[1]
     owner = sys.argv[2]
-    url = f"https://api.github.com/repos/{owner}/{repo_name}/commits"
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
     response = requests.get(url)
-    commits = response.json()
-    for commit in commits[:10]:
-        print(f"{commit.get('sha')}: {commit.get('commit').get('author').get('name')}")
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            sha = commit.get('sha')
+            author = commit.get('commit').get('author').get('name')
+            print(f"{sha}: {author}")
+    else:
+        print("Error fetching commits.")
+
+
+if __name__ == "__main__":
+    main()
