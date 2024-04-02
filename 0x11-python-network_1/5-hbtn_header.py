4e67a751192f2148a5e53207ceba449a1570dc71
Tsehafetizaz <wendetefe@gmail.com>
2024-04-02 06:40:48 -0700

    hello

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+"""Displays the value of the X-Request-Id response header using requests."""
+
 import requests
 import sys
 
-if __name__ == "__main__":
+
+def main():
+    """Main function to retrieve and display X-Request-Id."""
     url = sys.argv[1]
     response = requests.get(url)
     print(response.headers.get('X-Request-Id'))
+
+
+if __name__ == "__main__":
+    main()
