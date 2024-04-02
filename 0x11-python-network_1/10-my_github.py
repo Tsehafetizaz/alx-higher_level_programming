021c95ab11e383ed1f3faadebf134378e9a7b487
Tsehafetizaz <wendetefe@gmail.com>
2024-04-02 06:51:23 -0700

    hello

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+"""Uses the GitHub API to display user id using Basic Authentication."""
+
 import requests
 import sys
 
-if __name__ == "__main__":
+
+def main():
+    """Main function to fetch user id using GitHub API."""
     username = sys.argv[1]
-    token = sys.argv[2]
-    url = f"https://api.github.com/user"
-    response = requests.get(url, auth=(username, token))
-    print(response.json().get('id'))
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, password))
+    if response.status_code == 200:
+        print(response.json().get('id'))
+    else:
+        print("None")
+
+
+if __name__ == "__main__":
+    main()
