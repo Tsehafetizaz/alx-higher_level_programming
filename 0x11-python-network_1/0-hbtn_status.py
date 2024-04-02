8110b2b753c00cae5519ee38c89d60ea7727a13e
Tsehafetizaz <wendetefe@gmail.com>
2024-04-02 06:30:57 -0700

    hello

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,14 +3,17 @@
 
 import urllib.request
 
+
 def main():
     """Main function to fetch and display status."""
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen(
+            'https://alx-intranet.hbtn.io/status') as response:
         content = response.read()
         print("Body response:")
         print("\t- type:", type(content))
         print("\t- content:", content)
         print("\t- utf8 content:", content.decode('utf-8'))
+
 
 if __name__ == "__main__":
     main()
