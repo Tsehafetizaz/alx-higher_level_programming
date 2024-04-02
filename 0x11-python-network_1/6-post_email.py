0b79e33e4c778525d54f32425d9c6c71dfab7328
Tsehafetizaz <wendetefe@gmail.com>
2024-04-02 06:42:16 -0700

    hello

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""Sends a POST request with an email parameterponse body using requests."""
+
 import requests
 import sys
 
-if __name__ == "__main__":
+
+def main():
+    """Main function to send a POST request with email parameter."""
     url = sys.argv[1]
-    email = {'email': sys.argv[2]}
-    response = requests.post(url, data=email)
+    email = sys.argv[2]
+    response = requests.post(url, data={'email': email})
     print(response.text)
+
+
+if __name__ == "__main__":
+    main()
